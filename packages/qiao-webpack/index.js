3c9d7ccdedbef44860f87f72a08720127972bf96
uikoo9 <uikoo9@qq.com>
2023-09-05 21:31:13 +0800

    feat(qiao-webpack): add output and server

--- packages/qiao-webpack/index.js
+++ packages/qiao-webpack/index.js
@@ -1,1 +1,38 @@
-module.exports = require('./lib/qiao-webpack.js');
+// log util
+const logUtil = require('./lib/log.js');
+
+/**
+ * server
+ * @param {*} serverPath
+ * @param {*} port
+ * @returns
+ */
+exports.server = function (serverPath, port) {
+  if (!serverPath) {
+    logUtil.danger('qiao-webpack / server / need serverPath');
+    return;
+  }
+
+  return {
+    port: port || 5299,
+    static: serverPath,
+  };
+};
+
+/**
+ * output
+ * @param {*} outputPath
+ * @returns
+ */
+exports.output = function (outputPath) {
+  if (!outputPath) {
+    logUtil.danger('qiao-webpack / output / need outputPath');
+    return;
+  }
+
+  return {
+    clean: true,
+    path: outputPath,
+    filename: '[name].[contenthash:8].js',
+  };
+};
